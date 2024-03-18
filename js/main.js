80edb4030baa8c5c3e00d17d4ac773877fb99598
ecarnovsky <ecarnovsky@gmail.com>
2024-03-18 01:42:45 -0400

    Added a fetch to the api

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 
 navigator.geolocation.getCurrentPosition((position) => {
-    alert(position.coords.latitude + " " + position.coords.longitude )
+
+    fetch(`https://api.tomtom.com/search/2/poiSearch/nature trail.json?key=xsnwWh7MFr7v6GKbnrtoR9P6rrDJkXGU&lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        for(let i = 0; i < data.results.length; i++){
+          console.log(data.results[i].poi.name)
+        }
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+
   });
